80b8de08384fbe22aef9cb8238bc66f9b38d5f4c
faruk5266 <farukhussain5266@gmail.com>
2020-10-15 19:00:00 +0600

    add dashboard, and many more things

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import {
 import Home from './Components/Home/Home/Home';
 import Appointment from './Components/Appointment/Appointment/Appointment';
 import Login from './Components/Login/Login/Login';
+import Dashboard from './Components/DashBoard/Dashboard/Dashboard';
+import AllPatients from './Components/AllPatients/AllPatients';
 
 export const UserContext = createContext();
 
@@ -22,6 +24,12 @@ function App() {
         </Route>
         <Route path="/appointment">
             <Appointment></Appointment>
+        </Route>
+        <Route path="/dashboard/allpatients">
+            <AllPatients></AllPatients>
+        </Route>
+        <Route path="/dashboard/appointment">
+            <Dashboard></Dashboard>
         </Route>
         <Route path="/login">
           <Login></Login>
